4ebfc5f4b2e2417cbb17c4476e0ef00c61232b20
sampee40 <67319419+sampee40@users.noreply.github.com>
2020-10-05 23:23:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var car,wall;
+var speed,weight;
+
+
+
+function setup() {
+  createCanvas(1600,400);
+
+   car = createSprite(50,200,70,50);
+  wall = createSprite(1500,200,30,250);
+  
+  car.shapeColor="orange";
+wall.shapeColor="orange";
+}
+
+function draw() {
+  
+  background(0);
+ 
+  speed= random(55,90);
+  weight=random(400,1500);
+  car.velocityX=random(1,5);
+
+  if(wall.x-car.x<(car.width+wall.width)/2)
+  {
+  car.velocityX=0;
+  var deformation=0.5*weight*speed*speed/22509;
+  }
+  if(deformation>180)
+  {
+  car.shapeColor=color(255,0,0);
+  }
+  else if(deformation<180 && deformation>100)
+  {
+    car.shapeColor=color(230,230,0);
+  }
+  else if(deformation<100)
+  {
+    car.shapeColor=color(0,255,0);
+  }
+ 
+  drawSprites();
+}
